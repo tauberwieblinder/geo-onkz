552dcafc5b5a69d46603739f6856a04fad265a62
Chris Tauber <uk076830@student.uni-kassel.de>
2025-03-09 00:10:28 +0100

    add maven files

---
+++ src/test/java/de/tauberwieblinder/geo_onkz/GeoOnkzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tauberwieblinder.geo_onkz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoOnkzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
